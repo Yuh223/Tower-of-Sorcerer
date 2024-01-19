74f9a740b51bb12acafb21806fa1cc5ba9ade570
yuj223 <60333124+yuj223@users.noreply.github.com>
2024-01-18 23:28:56 -0500

    play.ts

--- src/game/play.ts
+++ src/game/play.ts
@@ -33,6 +33,150 @@ export function gameBuilder(level: number){
   }
   callfieldBook();
 }
+  if(level == 3){
+    sstore.levels=3;
+    levelConstructor(sstore.level3);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 4){
+    sstore.levels=4;
+    levelConstructor(sstore.level4);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 5){
+    sstore.levels=5;
+    levelConstructor(sstore.level5);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 6){
+    sstore.levels=6;
+    levelConstructor(sstore.level6);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 7){
+    sstore.levels=7;
+    levelConstructor(sstore.level7);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 8){
+    sstore.levels=8;
+    levelConstructor(sstore.level8);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 9){
+    sstore.levels=9;
+    levelConstructor(sstore.level9);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 10){
+    sstore.levels=10;
+    levelConstructor(sstore.level10);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 11){
+    sstore.levels=11;
+    levelConstructor(sstore.level11);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 12){
+    sstore.levels=12;
+    levelConstructor(sstore.level12);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 13){
+    sstore.levels=13;
+    levelConstructor(sstore.level13);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 14){
+    sstore.levels=14;
+    levelConstructor(sstore.level14);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 15){
+    sstore.levels=15;
+    levelConstructor(sstore.level15);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 16){
+    sstore.levels=16;
+    levelConstructor(sstore.level16);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 17){
+    sstore.levels=17;
+    levelConstructor(sstore.level17);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 18){
+    sstore.levels=18;
+    levelConstructor(sstore.level18);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 19){
+    sstore.levels=19;
+    levelConstructor(sstore.level19);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
+  if(level == 20){
+    sstore.levels=20;
+    levelConstructor(sstore.level20);
+    UI(sstore);
+    stage.gameMusic!.play();
+  }
+  callfieldBook();
+}
 export function UI(sstore:SStore){
   new Actor({
     appearance: new TextSprite({ center: true, face: "TimesNewRoman", color: "#140000", size: 30, z: 1 }, 
